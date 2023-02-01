e9fa079170dd85f5a56b29aad49d2d17738bb7d9
NurikStark <121919820+NurikStark@users.noreply.github.com>
2023-02-01 20:08:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+// module6_homework-2
+
+function primeNumb(num) {
+  if (num > 1) {
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+        return "Составное число";
+      }
+    }
+    return "Простое число";
+  } else {
+    return "Число должно быть больше 1";
+  }
+}
+console.log(primeNumb(150));
+
+// module6_homework-3
+
+function sum(x) {
+    return function(y) {
+        return x + y;
+    };
+}
+let f = sum(5);
+console.log( sum(5)(2), sum(2)(2), sum(40)(2) );
+// module6_homework-4
+const intervalId = setInterval(function (a, b) {
+
+   for(let i = 0; i <= b; i++){
+      if(i >= a && i <= b){
+        console.log(i)
+      }
+    }
+
+}, 1000, 5,  15); 
+// module6_homework-5
+
 const multiplication = (a, b) =>{
   const result = Math.pow(a, b);
   console.log(result)
